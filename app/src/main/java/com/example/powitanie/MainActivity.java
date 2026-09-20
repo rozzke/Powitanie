dc73786e0b2fda4e19eae8c4bbb9fd4126791691
Rozalia Cyrulinska <xxrozke@gmail.com>
2026-09-20 21:33:13 +0200

    Powitanie + Toast przy pustym polu

--- app/src/main/java/com/example/powitanie/MainActivity.java
+++ app/src/main/java/com/example/powitanie/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.powitanie;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private Button buttonGreet;
+    private TextView textViewGreeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editTextName = findViewById(R.id.editTextName);
+        buttonGreet = findViewById(R.id.buttonGreet);
+        textViewGreeting = findViewById(R.id.textViewGreeting);
+
+        buttonGreet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    Toast.makeText(MainActivity.this, R.string.empty_name, Toast.LENGTH_SHORT).show();
+                    textViewGreeting.setText("");
+                } else {
+                    String greeting = getString(R.string.greeting, name);
+                    textViewGreeting.setText(greeting);
+                }
+            }
         });
     }
 }
